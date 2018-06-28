31fef89ba4fc72a3527144f0f6a2c37fdf202478
Elias Kotlyar <elias.kotlyar@mediawave.de>
2018-06-28 17:41:22 +0200

    Compile

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 TOOLCHAIN=$(pwd)/../toolchain_4/bin
 export PATH=$TOOLCHAIN:$PATH
-#make distclean
+export CROSS_COMPILE=mips-linux-gnu-
+make distclean
 make isvp_t20_sfcnor_ddr128M_config
 make -n
