acce7b8cf219fabd19c952497672bdc8fcd6dcaf
Elias Kotlyar <elias.kotlyar@mediawave.de>
2018-06-29 11:59:05 +0200

    MMC

--- compile.sh
+++ compile.sh
@@ -2,6 +2,6 @@
 TOOLCHAIN=$(pwd)/../toolchain_5/bin
 export PATH=$TOOLCHAIN:$PATH
 export CROSS_COMPILE=mips-linux-gnu-
-#make distclean
-#make isvp_t20_sfcnor_ddr128M_config
-make
+make distclean
+make isvp_t20_sfcnor_ddr128M_config
+make V=1
